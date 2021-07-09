74d1cbdc6b9ef109e501fb9b82bebd127f9fd375
apchavan <ameyc96@gmail.com>
2021-07-09 11:21:55 +0530

    Added Go programs

---
+++ Variables/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a = "initial"
+    fmt.Println("a =", a)
+
+    var b, c int = 1, 2
+    fmt.Println("b =", b, ", c =", c)
+
+    var d = true
+    fmt.Println("boolean 'd' =", d)
+
+    var e int
+    fmt.Println("default value for 'int' =", e)
+
+    f := "apple"
+    fmt.Println("f =", f)
+}
