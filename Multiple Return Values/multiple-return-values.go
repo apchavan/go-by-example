bc1685e34f2f3eadb388a51e8ce9b8d81d4843f9
apchavan <ameyc96@gmail.com>
2021-07-09 15:35:29 +0530

    Added Go Programs

--- Multiple Return Values/multiple-return-values.go
+++ Multiple Return Values/multiple-return-values.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func vals() (int, int) {
-	return 3, 7
+    return 3, 7
 }
 
 func main() {
 
-	a, b := vals()
-	fmt.Println(a)
-	fmt.Println(b)
+    a, b := vals()
+    fmt.Println(a)
+    fmt.Println(b)
 
-	_, c := vals()
-	fmt.Println(c)
+    _, c := vals()
+    fmt.Println(c)
 }
