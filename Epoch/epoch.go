2d14addf178682ecb191578c5d144c02a22a0bfe
apchavan <ameyc96@gmail.com>
2021-07-12 19:01:30 +0530

    Added Go Programs

--- Epoch/epoch.go
+++ Epoch/epoch.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 func main() {
 
-	now := time.Now()
-	secs := now.Unix()
-	nanos := now.UnixNano()
-	fmt.Println(now)
+    now := time.Now()
+    secs := now.Unix()
+    nanos := now.UnixNano()
+    fmt.Println(now)
 
-	millis := nanos / 1000000  // 1 millisecond = 1,000,000 nanoseconds (or 1e+6 nanoseconds)
-	fmt.Println(secs)
-	fmt.Println(millis)
-	fmt.Println(nanos)
+    millis := nanos / 1000000  // 1 millisecond = 1,000,000 nanoseconds (or 1e+6 nanoseconds)
+    fmt.Println(secs)
+    fmt.Println(millis)
+    fmt.Println(nanos)
 
-	fmt.Println(time.Unix(secs, 0))
-	fmt.Println(time.Unix(0, nanos))
+    fmt.Println(time.Unix(secs, 0))
+    fmt.Println(time.Unix(0, nanos))
 }
